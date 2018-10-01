f84dc9c1a0399c4103d6856cf6e5dbe3a3562af8
Roman Rybalkin <rrybalkin@inbox.ru>
2018-10-01 11:14:41 +0300

    (NOTAG) SQL scripts improvement

--- scripts/init.sql
+++ scripts/init.sql
@@ -69,3 +69,13 @@ CREATE SEQUENCE sq_talons_id
   MAXVALUE 10000
   NOCACHE
   NOCYCLE;
+
+
+-- indexes on table VISITORS
+CREATE INDEX xif04_v ON visitors (association);
+
+-- indexes on table VISITOR_TALON
+CREATE INDEX xif05_tv ON visitor_talon (visitor_id);
+CREATE INDEX xif06_tv ON visitor_talon (talon_id);
+
+INSERT INTO associations VALUES(0, 'No group', null);
